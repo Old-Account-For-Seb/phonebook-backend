7e9ede29479c89d01ee0a02a325cce43b3f869b9
Sebastian-Sinclair <seb.lucas.sinclair@gmail.com>
2024-05-13 21:28:23 -0400

    3.16 finished

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.use(express.static('dist'))
 app.use(morgan('tiny'))
 app.use(cors())
 
-app.get('/', (request, response) => {
-    response.send('hello world')
-})
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
@@ -37,19 +33,15 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${personsNumber} people</p> <p>${date}</p>`)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-const generateId = () => {
-    return Math.floor(Math.random() * 5000)
-  }
-
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.post('/api/persons', (request, response) => {
@@ -64,6 +56,18 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
